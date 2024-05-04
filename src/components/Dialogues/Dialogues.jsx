ad67842e364c83e0f3699b18ad2f0da26bc45fc8
Kseniia Vakhrusheva <kseniia.vakhrusheva@gmail.com>
2024-05-04 07:57:44 +0200

    add dialogues and messages links and components

--- src/components/Dialogues/Dialogues.jsx
+++ src/components/Dialogues/Dialogues.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import s from "./Dialogues.module.css";
+import { NavLink } from "react-router-dom/cjs/react-router-dom";
 
-const Dialogues = () => {
+const DialogueItem = (props) => {
+    let path = "/dialogues/"+ props.id;
+    return <div className={s.dialogue + " " + s.active}>
+      <NavLink to={path}>{props.name}</NavLink>
+   </div>
+}
+
+const Message = (props) => {
+  return <div className={s.message}>{props.message}</div>
+}
+
+const Dialogues = (props) => {
   return (
-    <div className={s.content}>
-        Dialogues
+    <div className={s.dialogues}>
+      <div className={s.dialoguesItems}>
+       <DialogueItem name='Mike' id='1' />
+       <DialogueItem name='Lina' id='2' />
+       <DialogueItem name='Olga' id='3' />
+      </div>
+      <div className={s.messages}>
+        <Message message='Hi'/>
+        <Message message='Hi1'/>
+        <Message message='Hi2'/>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Dialogues;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 import s from "./Navbar.module.css";
+import { NavLink } from "react-router-dom/cjs/react-router-dom";
+import ProfileIcon from "../../image/profile.svg";
+import MessageIcon from "../../image/message.svg";
+import NewsIcon from "../../image/news.svg";
+import MusicIcon from "../../image/music.svg";
+import SettingsIcon from "../../image/settings.svg";
 
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <a href='/profile'>Profile</a>
+        <NavLink to="/profile" activeClassName={s.active}>
+          <img src={ProfileIcon} className={s.icon}></img>
+          <span>Profile</span>
+        </NavLink>
       </div>
       <div className={s.item}>
-        <a href='/dialogues'>Messages</a>
+        <NavLink to="/dialogues" activeClassName={s.active}>
+          <img src={MessageIcon} className={s.icon}></img>
+          <span>Messages</span>
+        </NavLink>
       </div>
       <div className={s.item}>
-        <a href="/news">News</a>
+        <NavLink to="/news" activeClassName={s.active}>
+          <img src={NewsIcon} className={s.icon}></img>
+          <span>News</span>
+        </NavLink>
       </div>
       <div className={s.item}>
-        <a href='/music'>Music</a>
+        <NavLink to="/music" activeClassName={s.active}>
+          <img src={MusicIcon} className={s.icon}></img>
+          <span>Music</span>
+        </NavLink>
       </div>
       <div className={s.item}>
-        <a href='settings'>Settings</a>
+        <NavLink to="settings" activeClassName={s.active}>
+          <img src={SettingsIcon} className={s.icon}></img>
+          <span>Settings</span>
+        </NavLink>
       </div>
     </nav>
   );
