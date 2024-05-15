8e6b04e309b75634c186e01578d467b5111bc481
Kseniia Vakhrusheva <kseniia.vakhrusheva@gmail.com>
2024-05-15 12:13:12 +0200

    refactor action creator and type

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
+
 let store = {
   _state: {
     profilePage: {
@@ -46,21 +49,30 @@ let store = {
   },
 
   dispatch(action) {
-    if (action.type === 'ADD-POST') {
-        let newPost = {
-            id: 5,
-            message: this._state.profilePage.newPostText,
-            likesCount: 0,
-          };
-          this._state.profilePage.posts.push(newPost);
-          this._state.profilePage.newPostText = "";
-          this._callSubscriber(this._state); 
-    } else if (action.type === 'UPDATE-NEW-POST-TEXT')  {
-        this._state.profilePage.newPostText = action.newText;
-        this._callSubscriber(this._state);
+    if (action.type === ADD_POST) {
+      let newPost = {
+        id: 5,
+        message: this._state.profilePage.newPostText,
+        likesCount: 0,
+      };
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = "";
+      this._callSubscriber(this._state);
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
     }
-  }
+  },
 };
+
+export const addPostActionCreator = () => ({
+  type: ADD_POST,
+});
+
+export const updateNewPostTextActionCreator = (text) => ({
+    type: UPDATE_NEW_POST_TEXT,
+    newText: text,
+});
 
 export default store;
 window.store = store;
